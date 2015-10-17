daf72eccc7b42ce654e4383a666672c98c4ce266
Saptaparna Bhattacharya <saptaparna@gmail.com>
2015-10-17 14:01:05 -0500

    CRAB3 Config

---
+++ QBH_ADD_MD_4_MQBH_5_n_3_MiniAOD.py
@@ -0,0 +1,108 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: step3 --conditions MCRUN2_74_V9 --fast -n 10 --eventcontent MINIAODSIM --runUnscheduled --filein file:fastsim.root -s PAT --datatier MINIAODSIM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 --mc
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('PAT')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('FastSimulation.Configuration.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('FastSimulation.Configuration.Geometries_MC_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+#    fileNames = cms.untracked.vstring('file:fastsim.root'),
+#    fileNames = cms.untracked.vstring('file:MLM_QBH_John_MD_4_MQBH_4_n_1_noHeader.root'),
+    fileNames = cms.untracked.vstring('root://cmsxrootd-site.fnal.gov//store/user/lpcmj/BlackHoleAnalysis/QBH_ADD_MD_4_MQBH_6_n_4/QBH_ADD_MD_4_MQBH_6_n_4/QBH_ADD_MD_4_MQBH_6_n_4/151014_035314/0000/QBH_MD_4_MQBH_6_n_0_53.root'),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet(
+    allowUnscheduled = cms.untracked.bool(True)
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('step3 nevts:10000'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.MINIAODSIMoutput = cms.OutputModule("PoolOutputModule",
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(4),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('MINIAODSIM'),
+        filterName = cms.untracked.string('')
+    ),
+    dropMetaData = cms.untracked.string('ALL'),
+    eventAutoFlushCompressedSize = cms.untracked.int32(15728640),
+    fastCloning = cms.untracked.bool(False),
+    fileName = cms.untracked.string('QBH_ADD_MD_4_MQBH_5_n_3_MiniAOD.root'),
+    outputCommands = process.MINIAODSIMEventContent.outputCommands,
+    overrideInputFileSplitLevels = cms.untracked.bool(True)
+)
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'MCRUN2_74_V9', '')
+
+# Path and EndPath definitions
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.MINIAODSIMoutput_step = cms.EndPath(process.MINIAODSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.endjob_step,process.MINIAODSIMoutput_step)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from FastSimulation.Configuration.MixingModule_Full2Fast
+from FastSimulation.Configuration.MixingModule_Full2Fast import prepareDigiRecoMixing 
+
+#call to customisation function prepareDigiRecoMixing imported from FastSimulation.Configuration.MixingModule_Full2Fast
+process = prepareDigiRecoMixing(process)
+
+# Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.postLS1Customs
+from SLHCUpgradeSimulations.Configuration.postLS1Customs import customisePostLS1 
+
+#call to customisation function customisePostLS1 imported from SLHCUpgradeSimulations.Configuration.postLS1Customs
+process = customisePostLS1(process)
+
+# End of customisation functions
+#do not add changes to your config after this point (unless you know what you are doing)
+from FWCore.ParameterSet.Utilities import convertToUnscheduled
+process=convertToUnscheduled(process)
+process.load('Configuration.StandardSequences.PATMC_cff')
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from PhysicsTools.PatAlgos.slimming.miniAOD_tools
+from PhysicsTools.PatAlgos.slimming.miniAOD_tools import miniAOD_customizeAllMC 
+
+#call to customisation function miniAOD_customizeAllMC imported from PhysicsTools.PatAlgos.slimming.miniAOD_tools
+process = miniAOD_customizeAllMC(process)
+
+# Automatic addition of the customisation function from PhysicsTools.PatAlgos.slimming.metFilterPaths_cff
+from PhysicsTools.PatAlgos.slimming.metFilterPaths_cff import miniAOD_customizeMETFiltersFastSim 
+
+#call to customisation function miniAOD_customizeMETFiltersFastSim imported from PhysicsTools.PatAlgos.slimming.metFilterPaths_cff
+process = miniAOD_customizeMETFiltersFastSim(process)
+
+# End of customisation functions
